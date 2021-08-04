574e5f20fb82d42945dca5f970ed204fbcf012d2
coffeecato <coffeecatox@gmail.com>
2021-08-04 10:45:46 +0800

    开始学习Custom SRP-Taking Control of Rendering 创建渲染流水线, 渲染不支持的shader, 完善编辑器显示

---
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer
+{
+    ScriptableRenderContext context;
+    const string bufferName = "Render Camera";
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+    Camera camera;
+    CullingResults cullingResults;
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+        PrepareForSceneWindow();
+        if (!Cull())
+        {
+            return;
+        }
+
+        Setup();
+        DrawVisibleGeometry();
+        DrawUnsupportedShaders();
+        DrawGizmos();
+        Submit();
+    }
+
+    void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        buffer.BeginSample(bufferName);
+        buffer.ClearRenderTarget(true, true, Color.clear);
+        ExecuteBuffer();
+    }
+    void Submit()
+    {
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
+        context.Submit();
+    }
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+    void DrawVisibleGeometry()
+    {
+        var sortingSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+
+        context.DrawSkybox(camera);
+
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+    }
+    bool Cull()
+    {
+        // ScriptableCullingParameters p
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+}
